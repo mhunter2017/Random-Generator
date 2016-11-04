8125e4077c2e522368e2d76755766aed3f2b3d2d
Morgaine <morgaineleagueol@gmail.com>
2016-11-04 11:32:19 -0700

    Initial Commit

---
+++ random_gen.py
@@ -0,0 +1,8 @@
+#menu
+lista = ['Yummy', 'Luxurious' 'Tasty', 'Aged', 'Aromatic', 'Appetizing', 'Balsamic', 'Torched', 'Buttered', 'Caramelized']
+listb = ['parmesan', 'tomato', 'olive oil', 'red snapper', 'salmon', 'marinara', 'mushroom', 'meatball', 'basil', 'spinach']
+listc = ['risotto', 'eggplant', 'spaghetti', 'pizza', 'rigatoni', 'tortelini', 'Bruschetta', 'ravioli', 'eggplant parmesan', 'carbonara']
+
+print random(lista)
+print random(listb)
+print random(listc)
